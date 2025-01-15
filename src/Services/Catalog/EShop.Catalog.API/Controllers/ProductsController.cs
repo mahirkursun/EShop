b43b93a8212ee051f876547ec36a4fe6c7920134
Mahir Kurşun <26101211@beko.com>
2025-01-15 12:49:17 +0300

    master

--- src/Services/Catalog/EShop.Catalog.API/Controllers/ProductsController.cs
+++ src/Services/Catalog/EShop.Catalog.API/Controllers/ProductsController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace EShop.Catalog.API.Controllers
 {
@@ -9,8 +8,8 @@ namespace EShop.Catalog.API.Controllers
     {
         public Task<IActionResult> GetProducts()
         {
-            var result = Ok(new { products = new[] { "A", "B", "C" } });
 
+            var result = Ok(new { products = new[] { "A", "B", "C" } });
             return Task.FromResult<IActionResult>(result);
         }
     }
